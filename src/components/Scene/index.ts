6877893c76d6ebba1eca8293b8a8aa1b848f63db
Mariano Eiberman <mariano.eiberman@gmail.com>
2024-11-22 09:58:31 -0300

    refactor to improve build

---
+++ src/components/Scene/index.ts
@@ -0,0 +1,4 @@
+export { GameOver } from '@/components/scene/game-over';
+export { Game as MainGame } from '@/components/scene/first-map';
+export { MainMenu } from '@/components/scene/main-menu';
+export { Preloader } from '@/components/scene/preloader';
--- src/components/game/main.ts
+++ src/components/game/main.ts
@@ -1,14 +1,8 @@
-
-import { GameOver } from '@/components/scene/game-over';
-import { Game as MainGame } from '@/components/scene/first-map';
-import { MainMenu } from '@/components/scene/main-menu';
 import GameUI from '@/components/game/game-ui';
 import { AUTO, Game } from 'phaser';
-import { Preloader } from '@/components/scene/preloader';
+import { GameOver, MainGame, MainMenu, Preloader } from "@/components/scene";
 import { Boot } from '@/components/game/boot';
 
-//  Find out more information about the Game Config at:
-//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
     width: 800,
